06e053bbabf3ac93d51d326075b4c1a2b105c35f
Hoodoo42 <create@hoodooverse.ca>
2022-07-09 10:20:28 -0600

    started working with loops

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let counter = 0;
+while(counter < 51){
+    console.log(counter);
+    counter = counter + 1;
+}
+
+counter = 0;
+while(counter > -51){
+    console.log(counter);
+    counter = counter -1;
+}
